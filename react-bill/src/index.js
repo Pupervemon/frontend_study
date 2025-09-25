e7f1b1f648e76cf126c769ba8ed8365f52e52564
fffun <3326449180@qq.com>
2025-09-25 15:59:38 +0800

    使用RTK管理账单列表

--- react-bill/src/index.js
+++ react-bill/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-
-//导入定制主题文件
-import './theme.css'
-
+import { Provider } from 'react-redux';
+import store from './store';
+import './theme.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router} />
+    <Provider store={store} >
+        <RouterProvider router={router} />
+    </Provider>
+
 );
