6d5a4aacafc3026e6d4771493c6ccafe7e3778fe
fffun <3326449180@qq.com>
2025-09-25 15:20:17 +0800

    完成主题色的定制化

--- react-bill/src/pages/Layout/index.js
+++ react-bill/src/pages/Layout/index.js
@@ -1,9 +1,14 @@
+import { Button } from 'antd-mobile'
 import { Outlet } from 'react-router-dom'
 const Layout = () => {
     return (
         <div>
             <Outlet />
             this is layout
+            <Button color='primary'>测试全局</Button>
+            <div>
+                <Button color='primary'>测试局部</Button>
+            </div>
         </div>
     )
 }
