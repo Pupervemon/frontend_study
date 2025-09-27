a844b323c2e7e50541ce9eb6cf2c24dd7da4bfd1
fffun <3326449180@qq.com>
2025-09-27 10:30:50 +0800

    统计区域静态结构搭建

--- react-bill/src/pages/Layout/index.js
+++ react-bill/src/pages/Layout/index.js
@@ -1,22 +1,61 @@
-import { Button } from 'antd-mobile'
-import { Outlet } from 'react-router-dom'
-import { useEffect } from 'react'
+import { TabBar } from 'antd-mobile'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
 import { useDispatch } from 'react-redux'
 import { getBillList } from '@/store/modules/billStore'
+import { useEffect } from 'react'
+import './index.scss'
+
+const tabs = [
+    {
+        key: '/',
+        title: '月度账单',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />,
+    },
+]
+
 const Layout = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getBillList())
     }, [dispatch])
+    // 切换菜单跳转路由
+    const navigate = useNavigate()
+    const switchRoute = (path) => {
+        console.log(path)
+        navigate(path)
+    }
     return (
-        <div>
-            <Outlet />
-            this is layout
-            <Button color='primary'>测试全局</Button>
-            <div>
-                <Button color='primary'>测试局部</Button>
+        <div className="kaLayout">
+            <div className="page">
+                {/* 二级路由出口 */}
+                <Outlet />
             </div>
+
+            <TabBar
+                className="tabbar"
+                onChange={switchRoute}
+            >
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                ))}
+            </TabBar>
         </div>
     )
 }
+
 export default Layout
